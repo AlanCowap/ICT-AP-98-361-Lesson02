b17386fe14543999153c0ce225094b2e41f7a881
Alan Cowap <spam@alancowap.com>
2016-09-29 16:09:52 +0100

    Add Properties to & remove constructor from Rectangle class, modify Program.cs

--- Lesson02/Program.cs
+++ Lesson02/Program.cs
@@ -6,11 +6,23 @@ using System.Threading.Tasks;
 
 namespace Lesson02
 {
+    /**
+ *  1.0 inital iteration of class
+ *  1.1 modify to work with version 1.1 of Lesson02.Rectangle class
+ *
+ */
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Lesson02");
+            //Create a Rectangle object, get it's area.
+            Rectangle rect = new Rectangle();
+            rect.Length = 10.0;
+            rect.Width = 20.0;
+            double area = rect.GetArea();
+            Console.WriteLine("Area of Rectangle: {0}", area);
         }
     }
 }
--- Lesson02/Rectangle.cs
+++ Lesson02/Rectangle.cs
@@ -6,16 +6,43 @@ using System.Threading.Tasks;
 
 namespace Lesson02
 {
+
+    /**
+     *  1.0 inital iteration of class
+     *  1.1 remove constructor, replace with Properties (inc get and set) Length & Width
+     *
+     */
+
     class Rectangle
     {
         private double length;
         private double width;
-
-        public Rectangle(double l, double w)
+        //Properties
+        public double Length    // convention is properties are public, and name begins with uppercase letter
         {
-            length = l;
-            width = w;
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if (value > 0.0)   // Properties allow for validation
+                    length = value; // 'value' is a keyword
+            }
         }
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (value > 0.0)
+                    width = value;
+            }
+        }
+
 
         public double GetArea()
         {
